6fa8b8b9de3c9c70bb44604ba0f5a8bba82991b7
melanieaqui <80889497+melanieaqui@users.noreply.github.com>
2023-07-12 16:30:31 +0800

    Mines have been placed randomly

--- Cell.py
+++ Cell.py
@@ -1,13 +1,60 @@
 from tkinter import Button
+import settings
+import random
 class Cell:
-    def __init__ (self, is_mine = False):
+    all = []
+    def __init__ (self,x,y, is_mine = False):
         self.is_mine = is_mine
         self.cell_btn_object = None
+        self.x = x
+        self.y = y
     
+        Cell.all.append(self)
     def create_btn_object (self, location):
         btn = Button(
-            location 
-            
+            location,
+            width = 5,
+            height =2
+
         )
-        self.cell_btn_object = btn;
+        #left click
+        btn.bind('<Button-1>',self.left_click)
+        
+        #right click
+        btn.bind('<Button-3>',self.right_click)
+
+        self.cell_btn_object = btn
     
+    def left_click(self,e):
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+            
+    def get_cell_by_axis(self, x, y):
+        #return a cell object based on x, y
+         for cell in Cell.all:
+             if cell.x == x and cell.y == y:
+                 return cell
+            
+    def show_cell(self):
+        print(self.get_cell_by_axis(0,0))
+    def show_mine(self):
+        
+        #change into bomb icon later
+        self.cell_btn_object.configure(bg='red')
+    def right_click(self,e):
+        print(e)
+        print("righ")
+        
+    @staticmethod
+    def randomize_mines():
+        picked_cells= random.sample(
+            Cell.all, settings.MINES_COUNT  
+        )
+        print(picked_cells) #for debug purposes remove later
+        for c in picked_cells:
+            c.is_mine = True
+            
+    def __repr__(self):
+        return f"cell({self.x},{self.y})"
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from tkinter import*;
 from Cell import Cell
+import settings
+
 root = Tk()
 
 #background color
 root.configure(bg='black')
 root.geometry('1000x600')
-root.title("Minesweeper")
+root.title("Sweeper Lens")
 root.resizable(False,False)
 top_frame=Frame(
     root, 
@@ -23,8 +25,12 @@ game_frame = Frame(
 
 game_frame.place(x=0, y= 100)
 
-c1 = Cell()
-c1.create_btn_object(game_frame)
-
+for i in range (settings.GRID_SIZE):
+    for j in range (settings.GRID_SIZE):
+        c = Cell(i,j)
+        c.create_btn_object(game_frame)
+        c.cell_btn_object.grid(column = i, row = j)
+        
+Cell.randomize_mines()
 # run the window
 root.mainloop()
